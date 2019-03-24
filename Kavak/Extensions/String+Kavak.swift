310aa22b58ba08dbe2d9ff02780810e3e3ae11e4
Jose Chourio <josemchourio@gmail.com>
2019-03-24 02:17:59 -0400

    Add a line to this extension file to match with the standard throughout the app

--- Kavak/Extensions/String+Kavak.swift
+++ Kavak/Extensions/String+Kavak.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 extension String {
+    
     func getHeight(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
